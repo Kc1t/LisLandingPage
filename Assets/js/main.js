81b0ff0c61fa080b5c40e1ea642479978f7784f7
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2022-10-08 01:31:38 -0300

    TrailerWork

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,3 +2,15 @@ $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
 });
 
+function executar(){
+    var meuVideo = document.getElementById("meuVideo");
+    
+    if(meuVideo.pause)
+    {
+        meuVideo.play();
+    }
+    else{
+        meuVideo.pause();
+    }
+
+}
