cfd936136bad8a449d330c8f2588449115b434f4
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-02-10 16:55:29 -0300

    trailer start

--- main.js
+++ main.js
@@ -24,6 +24,23 @@ toggleVideo.addEventListener("click", function () {
   isPlaying = !isPlaying;
 });
 
+const startVideo = document.getElementById("startVideo");
+
+startVideo.addEventListener("click", function () {
+  video.play();
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "KeyF") {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
+    }
+  }
+});
 
 // const video = document.getElementById("fullscreenVideo");
 // const videoContainer = document.getElementById("videoContainer");
